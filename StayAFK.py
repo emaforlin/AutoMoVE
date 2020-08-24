d591819b2c40794039831281aa8994f31e9543fa
Emaxx21 <emaforlin@gmail.com>
2020-08-24 11:22:40 -0300

    Sorting the code classes.

--- StayAFK.py
+++ StayAFK.py
@@ -8,34 +8,29 @@ moves = ['w','a','s','d']
 qty_moves = randint(3,10)
 
 
-def next_move(moves, qty):
-    next_move = []
+class Character():
+    def move(self, moves):
+        duration = randint(1,3)
+        for l in moves:
+            pyKey.press(key=l, sec=duration)
 
-    for l in range(0,qty):
-        what_move = randint(0,len(moves)-1)
-        #print("What move: " + str(what_move))
-        print(len(next_move))
+    def get_nextMove(self, moves, qty):
+        next_move = []
+        for l in range(0,qty):
+            what_move = randint(0,len(moves)-1)
+            #print("What move: " + str(what_move))
+            print(len(next_move))
 
-        if len(next_move) == 0:
-            next_move.append(moves[what_move])
-        elif moves[what_move] != next_move[len(next_move)-1]:
-            next_move.append(moves[what_move])
-        
+            if len(next_move) == 0:
+                next_move.append(moves[what_move])
+            elif moves[what_move] != next_move[len(next_move)-1]:
+                next_move.append(moves[what_move])
+        print(next_move)
+        return next_move
 
-    #print("Qty: " + str(qty))
-    print(next_move)
-    return next_move
-
-def move_character(moves):
-    duration = randint(1,3)
-
-    for l in moves:
-        pyKey.press(key=l, sec=duration)
+person = Character()
 
 while True:
-    try:
-#    next_move(moves, qty_moves)
-        move_character(next_move(moves, qty_moves))
-        sleep(30)
-    except:
-        print("Ha ocurrido un error.")
+    move = person.get_nextMove(moves, qty_moves)
+    person.move(move)
+    sleep(30)
