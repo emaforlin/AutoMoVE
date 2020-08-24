cab4d8c100c84e2dc31f5a5005e7a148ed686846
Emaxx21 <emaforlin@gmail.com>
2020-08-24 19:54:26 -0300

    More functions added

--- StayAFK.py
+++ StayAFK.py
@@ -1,5 +1,4 @@
 import pyKey
-import socket
 from random import randint
 from time import sleep
 
@@ -8,34 +7,81 @@ moves = ['w','a','s','d']
 qty_moves = randint(3,10)
 
 
-def next_move(moves, qty):
-    next_move = []
+class Character:
+    def move(self, moves):
+        try:
+            duration = randint(1,3)
+            for l in moves:
+                pyKey.press(key=l, sec=duration)
+        except KeyboardInterrupt:
+            print('Exiting...')
 
-    for l in range(0,qty):
-        what_move = randint(0,len(moves)-1)
-        #print("What move: " + str(what_move))
-        print(len(next_move))
+    def get_nextMove(self, moves, qty):
+        next_move = []
+        for l in range(0,qty):
+            what_move = randint(0,len(moves)-1)
+            #print("What move: " + str(what_move))
+            #print(len(next_move))
 
-        if len(next_move) == 0:
-            next_move.append(moves[what_move])
-        elif moves[what_move] != next_move[len(next_move)-1]:
-            next_move.append(moves[what_move])
-        
+            if len(next_move) == 0:
+                next_move.append(moves[what_move])
+            elif moves[what_move] != next_move[len(next_move)-1]:
+                next_move.append(moves[what_move])
+        #print(next_move)
+        return next_move
 
-    #print("Qty: " + str(qty))
-    print(next_move)
-    return next_move
+    def walk(self):
+        show_msg()
+        try:
+            while True:
+                sleep(1)
+                pyKey.press(key='w', sec=10)
+        except KeyboardInterrupt:
+            print('Exiting...')
 
-def move_character(moves):
-    duration = randint(1,3)
+    def controlled_walk(self, time):
+        show_msg()
+        try:
+            while True:
+                pyKey.press(key='w', sec=time)
+                pyKey.press(key='s', sec=time)
+        except KeyboardInterrupt:
+            print('Exiting...')
 
-    for l in moves:
-        pyKey.press(key=l, sec=duration)
 
-while True:
-    try:
-#    next_move(moves, qty_moves)
-        move_character(next_move(moves, qty_moves))
-        sleep(30)
-    except:
-        print("Ha ocurrido un error.")
+def show_msg():
+    print('''
+    --> The program init in 10 seconds, remember maximize the window.
+
+    --> Ctrl + C or Ctrl + Z to exit.
+    ''')
+    sleep(10)
+
+def ui():
+    print('''
+# Modes #
+0)  Random           --> This mode makes random moves on the character (every 30s).
+1)  Walk             --> This mode makes the character walk fordward indefinitely.
+2)  Controlled walk  --> This mode makes a walk for a time defined for you and get back the same time.
+99) Exit the program
+''')
+    person = Character()
+    choose = int(input("$~> "))
+    if choose == 0:
+        try:
+            show_msg()
+            while True:
+                move = person.get_nextMove(moves, qty_moves)
+                person.move(move)
+                sleep(30)
+        except KeyboardInterrupt:
+            print('Exiting...')
+    elif choose == 1:
+        person.walk()
+    elif choose == 2:
+        time = int(input('Enter the time (seconds): '))
+        person.controlled_walk(time)
+    elif choose == 99:
+        exit()
+
+ui()
